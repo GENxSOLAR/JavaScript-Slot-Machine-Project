0d7726db3f73cf87c8efd8614d976c5173d32256
Yash <yashvenkat2112@gmail.com>
2023-12-19 20:36:02 +0530

    initial commit

---
+++ G-Project.js
@@ -0,0 +1,169 @@
+////////////JAVASCRIPT PROJECT-1////////////
+//PROJECT NAME : SLOT MACHINE//
+
+/////WORKING/////
+// 1. Deposit some money
+// 2. Determine number of lines to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. Check if the user one
+// 6. Give the user thier winnings
+// 7. play again or end
+
+///CODING///
+const prompt = require("prompt-sync")(); //Importing prompt package
+
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+  A: 2,
+  B: 4,
+  C: 6,
+  D: 8,
+};
+
+const SYMBOL_VALUES = {
+  A: 5,
+  B: 4,
+  C: 3,
+  D: 2,
+};
+
+const deposit = () => {
+  while (true) {
+    const depositamount = prompt("Enter a deposit amount: ");
+    const numberDepositAmount = parseFloat(depositamount);
+
+    //NaN(Not a number):Checks if the enetered number is a number or not
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+      console.log("Invalid deposit amount, try again");
+    } else {
+      return numberDepositAmount;
+    }
+  }
+};
+
+const getNumberOfLines = () => {
+  while (true) {
+    const lines = prompt("Enter number of lines to bet on(1-3): ");
+    const numberOfLines = parseFloat(lines);
+
+    //NaN(Not a number):Checks if the enetered number is a number or not
+    if (isNaN(numberOfLines) || numberOfLines > 3 || numberOfLines <= 0) {
+      console.log("Invalid number of lines entered, try again");
+    } else {
+      return numberOfLines;
+    }
+  }
+};
+
+const getBet = (balance, lines) => {
+  while (true) {
+    const bet = prompt("Enter bet per line: ");
+    const numberBet = parseFloat(bet);
+
+    if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+      console.log("Enter a valid bet amount");
+    } else {
+      return numberBet;
+    }
+  }
+};
+
+const spin = () => {
+  const symbols = [];
+
+  for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+    for (let i = 0; i < count; i++) {
+      symbols.push(symbol);
+    }
+  }
+
+  const reels = [];
+
+  for (let i = 0; i < COLS; i++) {
+    reels.push([]);
+    const reelSymnbols = [...symbols];
+
+    for (let j = 0; j < ROWS; j++) {
+      const randomIndex = Math.floor(Math.random() * reelSymnbols.length);
+      const selectedsymbol = reelSymnbols[randomIndex];
+      reels[i].push(selectedsymbol);
+      reelSymnbols.splice(randomIndex, 1);
+    }
+  }
+  return reels;
+};
+
+const transpose = (reels) => {
+  const rows = [];
+
+  for (let i = 0; i < ROWS; i++) {
+    rows.push([]);
+    for (let j = 0; j < COLS; j++) {
+      rows[i].push(reels[j][i]);
+    }
+  }
+  return rows;
+};
+
+const printRows = (rows) => {
+  for (const row of rows) {
+    let rowString = "";
+
+    for (const [i, symbols] of row.entries()) {
+      rowString += symbols;
+      if (i != row.length - 1) {
+        rowString += " | ";
+      }
+    }
+    console.log(rowString);
+  }
+};
+
+const getWinnings = (rows, bet, lines) => {
+  let winnings = 0;
+
+  for (let row = 0; row < lines; row++) {
+    const symbols = rows[row];
+    let allSame = true;
+
+    for (const symbol of symbols) {
+      if (symbol != symbols[0]) {
+        allSame = false;
+        break;
+      }
+    }
+    if (allSame) {
+      winnings += bet * SYMBOL_VALUES[symbols[0]];
+    }
+  }
+  return winnings;
+};
+
+const game = () => {
+  let balance = deposit();
+
+  while (true) {
+    console.log("You have a balance of $" + balance);
+    const numberofLines = getNumberOfLines();
+    const bet = getBet(balance, numberofLines);
+    balance -= bet * numberofLines;
+    const reels = spin();
+    const rows = transpose(reels);
+    printRows(rows);
+    const winnings = getWinnings(rows, bet, numberofLines);
+    balance += winnings;
+    console.log("You won, $" + winnings.toString());
+
+    if (balance <= 0) {
+      console.log("Insufficient balance");
+      break;
+    }
+
+    const playAgain = prompt("Doy you want to play again (y/n)");
+    if (playAgain != "y") break;
+  }
+};
+game();
